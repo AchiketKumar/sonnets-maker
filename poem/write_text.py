32bb9eba9e172e58722e0f4e4c0dd7480c67b3a2
AchiketKumar <55083712+AchiketKumar@users.noreply.github.com>
2019-11-03 19:12:26 +0530

    update

--- poem/write_text.py
+++ poem/write_text.py
@@ -4,21 +4,19 @@ import numpy as np
 from keras.models import Sequential
 
 
-model = load_model('/Users/nimishmishra/Desktop/Sonnets22.model')
+model = load_model('model38.model')
 print('Lets go')
-string_mapped = X[99]
+string_mapped = X[67]
 
 full_string = [int_to_vocab[value] for value in string_mapped]
 # generating characters
-for i in range(1000):
+for i in range(5000):
+    print(i)
     x = np.reshape(string_mapped,(1,len(string_mapped), 1))
     x = x / float(len(characters))
     pred_index = np.argmax(model.predict(x, verbose=0))
     full_string.append(int_to_vocab[pred_index])
     string_mapped.append(pred_index)
-    # Let's remove the first character. Logic flows like:
-    # Pick the first 100 characters(0-99) and predict the next character.
-    # Then pick the (1-100) characters and predict the next
     string_mapped = string_mapped[1:len(string_mapped)]
 
 txt=""
